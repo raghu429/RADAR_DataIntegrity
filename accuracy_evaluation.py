0db70c1ab5044456a486636fd80337b5ae270318
R Chang <raghu.changalva@gmail.com>
2020-06-05 09:25:04 -0400

    added the visualization and accuracy evaluation files with work scope defined in comments

---
+++ accuracy_evaluation.py
@@ -0,0 +1,7 @@
+# #** This piece of code contants methods to evaluate the 2D QIM embedding for the given Udacity data-set. We can further split this file into three different files if needed (most of the previously developed code could be re-used)
+# First - read the data file and generate a separate binary file with just the radar data
+# Add noise to the data if required (uniform or Gaussian with variable delta)
+# simulate the attack vectors, like add, delete or modify the random line items in the above generated file
+# get the detections out of the modified files and report the false alarms, error rate and accuracy (if the algorithm is able to detect the location of the modified data)
+# **#
+
---
+++ visualization_functions.py
@@ -0,0 +1,4 @@
+# # #** This piece of code contants methods to visualize the following
+# RMSE plots for multiple values of the R matrix and radar data generator sigma
+# A route display and the predicted value display for the original and encoded data
+# # **#
