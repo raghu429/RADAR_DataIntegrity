12063ab4b979758194f7325579ee15fb25ab6913
R Chang <raghu.changalva@gmail.com>
2020-06-06 21:13:21 -0400

    the end to end pipe-line implemented and tested for add, del and modify functions. Code read from data_x.txt only the RADAR data and encodes it, adds required noise and then tampers it with give attack vector and then decodes it and checks if the attack vectors are localized

--- accuracy_evaluation.py
+++ accuracy_evaluation.py
@@ -1,5 +1,10 @@
 # -*- coding: utf-8 -*-
 import numpy as np
+import random
+
+from tools import polar_to_cartesian, cartesian_to_polar
+from twoD_QIM import qim_quantize_twobits, getPointCloud_from_quantizedValues, qim_decode, NUMBITS, qim_dummy_encoded_pc, get_tamperedindices_twobits
+
 
 # #** This piece of code contants methods to evaluate the 2D QIM embedding for the given Udacity data-set. We can further split this file into three different files if needed (most of the previously developed code could be re-used)
 # First - read the data file and generate a separate binary file with just the radar data
@@ -8,7 +13,61 @@ import numpy as np
 # get the detections out of the modified files and report the false alarms, error rate and accuracy (if the algorithm is able to detect the location of the modified data)
 # **#
 
-#these three fucntions check for the midding indices, added indices and modified indices assuming the input is a list of repeating pattens
+
+def radarDataExtractor(filePath):
+    with open(filePath) as read_file:
+        # read_file = open(filePath, "R")
+        radar_data = []
+        for line in read_file:
+            data = line.split()
+            if(data[0] == 'R'):
+                [x, y, vx, vy] = polar_to_cartesian(float(data[1]),float(data[2]),float(data[3]))
+                radar_data.append([x, y])
+    return radar_data
+
+def tamperRadarAddUniformNoise(noise_sigma, data_in):
+    uniform_noise_added_data = data_in + np.random.uniform(-noise_sigma, noise_sigma, 2*len(data_in)).reshape(-1,2)
+    return uniform_noise_added_data
+
+
+def tamperRadarAddTracklets(data_in):
+    data_out = data_in
+    #for now this is fixed but this can be variable as-well
+    copy_row_num = 2
+    add_location_list = []
+    for i in range (1,2):
+        # print('data length ={}'.format(np.shape(data_in)[0]))
+        r1 = random.randint(0, np.shape(data_in)[0]-2)
+        #copy a rwo element from the data
+        copy_row = data_in[copy_row_num+i]
+        #paste it at a random location
+        data_out = np.insert(data_out, r1, copy_row, axis = 0)
+        add_location_list.append(r1)
+    return add_location_list, data_out
+
+def tamperRadarDeleteTracklets(data_in):
+    data_out = data_in
+    del_location_list = []
+    for i in range (1,2):
+        r1 = random.randint(0, np.shape(data_in)[0]-2)
+        #delete a rwo element from the data
+        data_out =  np.delete(data_out, r1, axis = 0)
+        del_location_list.append(r1)
+    return del_location_list, data_out
+
+def tamperRadarModifyTracklets(data_in):
+    data_out = data_in
+    mod_location_list = []
+    for i in range (1,2):
+        r1 = random.randint(0, np.shape(data_in)[0]-2)
+        #modify a rwo element in the data
+        data_out[r1] =  data_out[r1+1]
+        mod_location_list.append(r1)
+    return mod_location_list, data_out
+
+
+
+#these three functions check for the missing indices, added indices and modified indices assuming the input is a list of repeating pattens
 
 def findDeletedIndices(gt_list, mod_list):
     #here the assumption is that the lenth of the mod_list < gt_list
@@ -56,26 +115,82 @@ def findAddedIndices(gt_list, mod_list):
 
 
 if __name__ == '__main__':
-    #test missing
-    print('******Deleted Elements Test******')
-    list_orig = [1,2,3,1,2,3,1,2,3,1,2]
-    list_mod = [1,1,2,1,1,2]
-    missing_indices = findDeletedIndices(list_orig, list_mod)
-    print('missing indices = {}'.format(missing_indices))
-    print('\n')
+    # #test missing
+    # print('******Deleted Elements Test******')
+    # list_orig = [1,2,3,4,1,2,3,4,1,2,3,4,1,2]
+    # list_mod = [1,1,2,1,1,2]
+    # missing_indices = findDeletedIndices(list_orig, list_mod)
+    # print('missing indices = {}'.format(missing_indices))
+    # print('\n')
 
-    #test different
-    print('*****Modified Elements Test******')
-    list_orig = [1,2,3,1,2,3,1,2,3,1,2]
-    list_mod = [1,2,2,1,2,3,1,2,3,1,3]
-    different_indices = findModifiedIndices(list_orig, list_mod)
-    print('different indices = {}'.format(different_indices))
-    print('\n')
+    # #test different
+    # print('*****Modified Elements Test******')
+    # list_orig = [1,2,3,1,2,3,1,2,3,1,2]
+    # list_mod = [1,2,2,1,2,3,1,2,3,1,3]
+    # different_indices = findModifiedIndices(list_orig, list_mod)
+    # print('different indices = {}'.format(different_indices))
+    # print('\n')
 
-    #test addition
-    print('*****Modified Elements Test******')
-    list_orig = [1,2,3,1,2,3,1,2,3,1,2]
-    list_mod = [1,5,2,3,1,2,3,0,1,2,3,1,2]
-    added_indices = findAddedIndices(list_orig, list_mod)
-    print('added indices = {}'.format(added_indices))
-    print('\n')
+    # #test addition
+    # print('*****Modified Elements Test******')
+    # list_orig = [1,2,3,1,2,3,1,2,3,1,2]
+    # list_mod = [1,2,2,3,1,2,3,0,1,2,3,1,2]
+    # added_indices = findAddedIndices(list_orig, list_mod)
+    # print('added indices = {}'.format(added_indices))
+    # print('\n')
+
+
+    #extract radar data
+    radar_data_predictions = radarDataExtractor("data/data-2.txt")
+    print('original data:', radar_data_predictions)
+    radar_data_predictions = np.array([radar_data_predictions]).reshape(-1,2)
+    #encode the data for a given step-size
+    voxel_halfdelta = qim_quantize_twobits(radar_data_predictions)
+    voxel_halfdelta_npy = np.array([voxel_halfdelta]).reshape(-1,2)
+    print('quant encoded shape', voxel_halfdelta_npy.shape, voxel_halfdelta_npy)
+    
+    qim_encoded_pointcloud = getPointCloud_from_quantizedValues(  np.copy(voxel_halfdelta_npy))
+    print('encoded data', qim_encoded_pointcloud)
+
+    pred_plusNoise = tamperRadarAddUniformNoise(0.001, qim_encoded_pointcloud)
+    print('noise added pc={}'.format(pred_plusNoise))
+    
+    #open the encoded data and tamper it by adding noise and attack vectors
+    # added_indices, pred_plus_added = tamperRadarAddTracklets(pred_plusNoise)
+    added_indices, pred_plus_added = tamperRadarDeleteTracklets(pred_plusNoise)
+    # added_indices, pred_plus_added = tamperRadarModifyTracklets(pred_plusNoise)
+
+    
+    print('added indices:{}'.format(added_indices))
+    print('modified data:{}'.format(pred_plus_added))
+    #decode the tampered point cloud
+
+    decoded_CB, decoded_quantized_values = qim_decode(np.copy(pred_plus_added))
+    print('decoded codebook', decoded_CB)
+    decoded_codebook = np.array([decoded_CB]).reshape(-1,NUMBITS)
+
+    #calculate the BER
+    encoded_cb = qim_dummy_encoded_pc(len(radar_data_predictions.reshape(-1,NUMBITS)))
+    print('encoded cb', encoded_cb)
+    # uncomment below to test if the tamper index finder is working
+    #encoded_cb[2] = [1,1]
+    #encoded_cb[4] = [0,1]
+    
+    tampered_indices, b_errorRate = get_tamperedindices_twobits(decoded_codebook, encoded_cb)
+    print('tampered indices', tampered_indices)
+
+    #verify the accuracy
+    modified_list = np.packbits(decoded_codebook, axis = 1)
+    print('modified_list={}'.format(modified_list))
+    groundtruth_list = np.packbits(encoded_cb, axis = 1)
+    print('gt_list={}'.format(groundtruth_list))
+
+    # added_indices_recovered = findAddedIndices(groundtruth_list, modified_list)
+    # added_indices_recovered = findModifiedIndices(groundtruth_list, modified_list)
+    added_indices_recovered = findDeletedIndices(groundtruth_list, modified_list)
+    
+    print('recovered indices:{}'.format(added_indices_recovered))
+    
+    if(np.all(added_indices_recovered==added_indices)):
+        print('you got it buddy! finally..phew!!!!)')
+
