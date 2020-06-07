6d293e78c7dff25dc9109a5af133e9bfcda45d81
R Chang <raghu.changalva@gmail.com>
2020-06-07 13:02:33 -0400

    added the plots strategy for paper in comments

--- visualization_functions.py
+++ visualization_functions.py
@@ -1,4 +1,11 @@
 # # #** This piece of code contants methods to visualize the following
 # RMSE plots for multiple values of the R matrix and radar data generator sigma
 # A route display and the predicted value display for the original and encoded data
+# List of plots for the paper
+# 1. six plots: Three plots each for px, py: RMSE for clean and encoded frames at different variances of the R matrix (x axis) and generator data (y-axis) for a given step size
+# 2. Localization accuracy box plots for multiple runs of same data at different noise sigmas on x-axis (step <threshold, ==threshold, > threshold)
+# one for each (delete, add and modify)
+# 3. BER box plots for different data frames at different stepsizes
+# 4. False alaram rate of different frames at different step sizes
+# 5. prediction sample: GT, prediction without encoding and with encoding)
 # # **#
